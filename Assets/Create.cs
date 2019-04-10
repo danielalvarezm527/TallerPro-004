bed8449dc6c65c203fdcee2ddf81e060b547b333
Daniel Alvarez <danielalvarezmoncada527@gmail.com>
2019-04-10 15:29:54 -0500

    AddProject

---
+++ Assets/Create.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using zom = NPC.Enemy;
+using villa = NPC.Ally;
+
+public class Create : MonoBehaviour
+{
+    public TextMeshProUGUI numeroZombies;
+    public TextMeshProUGUI numeroAldeanos;
+    public int numZombies;
+    public int numAldeanos;
+    public GameObject[] Zom,Villa;
+    void Start()
+    {
+        new CrearInstancias();
+
+    }
+
+
+    /// <summary>
+    /// retroalimentacion del canvas para saber cuantos aldeanos y zombies hay en la scena
+    /// </summary>
+    private void Update()
+    {
+        Zom = GameObject.FindGameObjectsWithTag("Zombie");
+        Villa = GameObject.FindGameObjectsWithTag("Villager");
+        foreach (GameObject item in Zom)
+        {
+            numZombies = Zom.Length;
+        }
+        foreach (GameObject item in Villa)
+        {
+            numAldeanos = Villa.Length;
+        }
+
+        if(Villa.Length == 0)
+        {
+            numeroAldeanos.text = 0.ToString();
+        }
+        else
+        {
+            numeroAldeanos.text = numAldeanos.ToString();
+        }
+
+        numeroZombies.text = numZombies.ToString();
+    }
+}
+/// <summary>
+/// Creamos una cantidad al azar de instancias al azar y le agregamos un componente
+/// </summary>
+ class CrearInstancias 
+{
+    public GameObject cube;
+    public readonly int minInstancias = Random.Range(5, 16);
+    int selector = 0;
+    const int MAX = 26;
+    public CrearInstancias()
+    {
+        for (int i = 0; i < Random.Range(minInstancias,MAX); i++)
+        {
+            
+            if (selector == 0)
+            {
+                cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.AddComponent<Camera>();
+                cube.AddComponent<Heroe>();
+                cube.AddComponent<Heroe.MirarH>();
+                cube.AddComponent<Heroe.MoverH>();
+                cube.transform.position = new Vector3(Random.Range(-20, 21), 0, Random.Range(-20, 21));
+                selector += 1;
+            }
+
+            int selec = Random.Range(selector, 3);
+
+            if (selec == 1)
+            {
+                cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.AddComponent<villa.Villager>();
+                cube.transform.position = new Vector3(Random.Range(-20, 21), 0, Random.Range(-20, 21));
+            }
+            if (selec == 2)
+            {
+                cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.AddComponent<zom.Zombie>();
+                cube.transform.position = new Vector3(Random.Range(-20, 21), 0, Random.Range(-20, 21));
+            }
+        }
+    }
+}
---
+++ Assets/Heroe.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using zom = NPC.Enemy;
+using villa = NPC.Ally;
+
+/// <summary>
+/// calculamos las distancias con respecto a las demas entidades
+/// </summary>
+public class Heroe : MonoBehaviour
+{
+    float distancia;
+    float distanciaZ;
+    public float time;
+    public TextMeshProUGUI textoZombie;
+    public TextMeshProUGUI textoAldeano;
+    GameObject[] aldeanos, zombie;
+
+    DatosVillager datosAldeano = new DatosVillager();
+    DatosZombie datosZombie = new DatosZombie();
+
+    /// <summary>
+    /// asignamos datos basicos del heroe
+    /// </summary>
+    void Start()
+    {
+        Rigidbody hero = this.gameObject.AddComponent<Rigidbody>();
+        this.gameObject.tag = "Hero";
+        this.gameObject.name = "Hero";
+        hero.constraints = RigidbodyConstraints.FreezeAll;
+        hero.useGravity = false;
+        StartCoroutine(BuscaEntidades());
+        textoZombie = GameObject.FindGameObjectWithTag("TextZombie").GetComponent<TextMeshProUGUI>();
+        textoAldeano = GameObject.FindGameObjectWithTag("TextAldeano").GetComponent<TextMeshProUGUI>();
+    }
+
+    /// <summary>
+    /// contamos un tiempo
+    /// </summary>
+    public void Update()
+    {
+        time += Time.fixedDeltaTime;
+    }
+
+    /// <summary>
+    /// calculamos la distancia de los objetos con respecto al heroe para hacer la retroalimentacon por medio del canvas 
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator BuscaEntidades()
+    {
+        zombie = GameObject.FindGameObjectsWithTag("Zombie");
+        aldeanos = GameObject.FindGameObjectsWithTag("Villager");
+
+        // retroalimentacion para el aldeano
+        foreach (GameObject item in aldeanos)
+        {
+            yield return new WaitForEndOfFrame();
+            villa.Villager componenteAldeano = item.GetComponent<villa.Villager>();
+            if (componenteAldeano != null)
+            {              
+                distancia = Mathf.Sqrt(Mathf.Pow((item.transform.position.x - transform.position.x), 2) + Mathf.Pow((item.transform.position.y - transform.position.y), 2) + Mathf.Pow((item.transform.position.z - transform.position.z), 2));
+                if (distancia < 5f)
+                {
+                    time = 0;
+                    datosAldeano = item.GetComponent<villa.Villager>().datosAldeano;
+                    textoAldeano.text = "Hola, soy " + datosAldeano.nombre + " y tengo " + datosAldeano.edad.ToString() + " años";
+                }
+                if (time > 3)
+                {
+                    textoAldeano.text = " ";
+                }
+            }
+        }
+
+        // retroalimentacion para el zombie
+        foreach (GameObject itemZ in zombie)
+        {
+            yield return new WaitForEndOfFrame();
+            zom.Zombie componenteZombie = itemZ.GetComponent<zom.Zombie>();
+            if (componenteZombie != null)
+            {              
+                distanciaZ = Mathf.Sqrt(Mathf.Pow((itemZ.transform.position.x - transform.position.x), 2) + Mathf.Pow((itemZ.transform.position.y - transform.position.y), 2) + Mathf.Pow((itemZ.transform.position.z - transform.position.z), 2));
+                if (distanciaZ < 5f)
+                {
+                    time = 0;
+                    datosZombie = itemZ.GetComponent<zom.Zombie>().datosZombie;
+                    textoZombie.text = "Waaaarrrr quiero comer " + datosZombie.gusto;
+                }
+                if (time > 3)
+                {
+                    textoZombie.text = " ";
+                }
+            }
+        }
+        yield return new WaitForSeconds(0.1f);
+        StartCoroutine(BuscaEntidades());
+    }
+
+    /// <summary>
+    /// calse encargada de el movimiento del heroe
+    /// </summary>
+    public class MoverH : MonoBehaviour
+    {
+
+        Velocidad velocidad;
+
+        private void Start()
+        {
+            velocidad  = new Velocidad(Random.Range(0.25f, 0.5f));
+        }
+
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                this.gameObject.transform.Translate(0, 0, velocidad.velo);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                this.gameObject.transform.Translate(0, 0, -velocidad.velo);
+            }
+        }
+    }
+
+    /// <summary>
+    /// calse encargada de la rotacion del heroe
+    /// </summary>
+    public class MirarH : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                this.gameObject.transform.Rotate(0, -3, 0);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                this.gameObject.transform.Rotate(0, 3, 0);
+            }
+        }
+    }
+}
+
+/// <summary>
+/// calse para la velocidad read only del heroe
+/// </summary>
+public class Velocidad
+{
+    public readonly float velo;
+    public Velocidad(float vel)
+    {
+        velo = vel;
+    }
+}
